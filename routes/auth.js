14433a99e572da60d0703afa2889b06bbdf21053
Krzysztof Matyga <krzysztof.matyga@noinputsignal.com>
2023-08-24 21:07:00 +0200

    auth-check and admin-check routes added

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,12 @@ const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get('/auth-check', requireSignin, (req, res) => {
+  res.json({ ok: true });
+});
+router.get("/admin-check", requireSignin, isAdmin, (req, res) => {
+  res.json({ok: true});
+})
 
 //test
 router.get("/secret", requireSignin, isAdmin, secret)
